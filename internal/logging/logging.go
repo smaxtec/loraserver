2127d5a2d66e65f8c74bf88967e8710b0920f3c3
Orne Brocaar <info@brocaar.com>
2019-09-11 09:54:22 +0200

    Implement context ID logging for AS gRPC client.

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -2,12 +2,18 @@ package logging
 
 import (
 	"context"
+	"path"
+	"time"
 
 	"github.com/gofrs/uuid"
+	grpc_logging "github.com/grpc-ecosystem/go-grpc-middleware/logging"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // ContextKey defines the context key type.
@@ -15,6 +21,10 @@ type ContextKey string
 
 // ContextIDKey holds the key of the context ID.
 const ContextIDKey ContextKey = "ctx_id"
+
+type contextIDGetter interface {
+	GetContextId() []byte
+}
 
 // UnaryServerCtxIDInterceptor adds the ContextIDKey to the context and sets
 // it as a log field.
@@ -29,4 +39,61 @@ func UnaryServerCtxIDInterceptor(ctx context.Context, req interface{}, info *grp
 	})
 
 	return handler(ctx, req)
+}
+
+func UnaryClientCtxIDInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	startTime := time.Now()
+	err := invoker(ctx, method, req, reply, cc, opts...)
+
+	code := grpc_logging.DefaultErrorToCode(err)
+	level := grpc_logrus.DefaultCodeToLevel(code)
+	logFields := clientLoggerFields(ctx, method, reply, err, code, startTime)
+
+	levelLogf(log.WithFields(logFields), level, "finished client unary call")
+
+	return err
+}
+
+func clientLoggerFields(ctx context.Context, fullMethodString string, resp interface{}, err error, code codes.Code, start time.Time) logrus.Fields {
+	service := path.Dir(fullMethodString)[1:]
+	method := path.Base(fullMethodString)
+
+	fields := logrus.Fields{
+		"system":        "grpc",
+		"span.kind":     "client",
+		"grpc.service":  service,
+		"grpc.method":   method,
+		"grpc.duration": time.Since(start),
+		"grpc.code":     code.String(),
+	}
+
+	if getter, ok := resp.(contextIDGetter); ok {
+		var ctxID uuid.UUID
+		copy(ctxID[:], getter.GetContextId())
+
+		fields["ctx_id"] = ctxID
+	}
+
+	if err != nil {
+		fields[logrus.ErrorKey] = err
+	}
+
+	return fields
+}
+
+func levelLogf(entry *logrus.Entry, level logrus.Level, format string, args ...interface{}) {
+	switch level {
+	case logrus.DebugLevel:
+		entry.Debugf(format, args...)
+	case logrus.InfoLevel:
+		entry.Infof(format, args...)
+	case logrus.WarnLevel:
+		entry.Warningf(format, args...)
+	case logrus.ErrorLevel:
+		entry.Errorf(format, args...)
+	case logrus.FatalLevel:
+		entry.Fatalf(format, args...)
+	case logrus.PanicLevel:
+		entry.Panicf(format, args...)
+	}
 }
